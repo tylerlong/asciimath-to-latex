3f454f2711cce3b408da8ba3ccab73abc64c46d6
Tyler Long <tyler4long@gmail.com>
2016-11-20 20:38:33 +0800

    First working version

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: __dirname,
+    filename: "index.bundle.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'stage-0'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new uglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+};
