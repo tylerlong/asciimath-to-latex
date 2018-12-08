ab84eb65193640006c8b575789945e815327c130
Max Lay <kalda341@gmail.com>
2018-12-09 11:17:35 +1300

    Use \begin and \end for matricies

--- index.js
+++ index.js
@@ -719,7 +719,7 @@ function AMTparseExpr(str, rightbracket) {
       if (right == ')' || right == ']') {
         var left = newFrag.charAt(6);
         if ((left == '(' && right == ')' && symbol.output != '}') || (left == '[' && right == ']')) {
-          var mxout = '\\matrix{';
+          var mxout = '\\begin{matrix}';
           var pos = new Array(); //position of commas
           pos.push(0);
           var matrix = true;
@@ -788,7 +788,7 @@ function AMTparseExpr(str, rightbracket) {
               mxout += subarr.join('&');
             }
           }
-          mxout += '}';
+          mxout += '\\end{matrix}';
 
           if (matrix) { newFrag = mxout; }
         }
